0ac87c5f5468f6f10a1eed16c69ee0ba5741d2f3
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2020-01-22 21:53:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string= string.upcase 
+  if string != string.upcase 
     return HUH?! SPEAK UP, SONNY!.
+    elsif string == string.upcase
+    return 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
